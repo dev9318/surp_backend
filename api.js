39207c38d6743e1755f38b60017700e9070c0790
dev9318 <dev.moxaj@gmail.com>
2021-06-08 14:25:46 +0530

    add .env and database URI

--- api.js
+++ api.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const dbURI = process.env.dbURI;
+
 // the express app
 const app = express();
 
