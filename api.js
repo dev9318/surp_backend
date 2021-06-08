dadd971a71172694a3337cf7e5204be2cd153622
dev9318 <dev.moxaj@gmail.com>
2021-06-08 15:42:13 +0530

    Add connection to database

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const dbURI = process.env.dbURI;
 
@@ -11,7 +12,9 @@ const app = express();
 // set up body parser
 app.use(bodyParser.json());
 
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(process.env.port || 4000, function(){
+    console.log('app is now listening for requests and is connected to mongodb');
+}))
+  .catch(err => console.log(err));
 
-app.listen(process.env.port || 4000, function(){
-    console.log('app is now listening for requests');
-});
